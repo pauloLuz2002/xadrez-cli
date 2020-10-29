c494c83cecbcea41be3213b215422d89af1ae5a4
paulo.luz <paulo.luz@linx.com>
2020-10-29 19:03:54 -0300

    Criação da classe posição

--- xadrez-cli/Program.cs
+++ xadrez-cli/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_cli {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
